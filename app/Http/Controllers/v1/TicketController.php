131f259c31be8358515d29a78f2407c85a758c26
ShineDezign <gauravch.shinedezign@gmail.com>
2022-02-19 17:34:08 +0530

    add user info and services

--- app/Http/Controllers/v1/TicketController.php
+++ app/Http/Controllers/v1/TicketController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\v1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Traits\SendResponseTrait;
-use App\Models\{User, Role};
+use App\Models\{User, Role, Order};
 
 class TicketController extends Controller
 {
@@ -36,7 +36,7 @@ class TicketController extends Controller
                         if($order->user_server){
                             $serviceProvider = $serviceProvider."(".$order->user_server->domain.")";
                         }
-                        array_push($serviceArray, ['id' => jsencode_userdata($order->id), 'order_id' => $order->order_id,'serviceProviderId' => jsencode_userdata($order->company_id), 'serviceProvider' => $serviceProvider]);
+                        array_push($serviceArray, ['id' => jsencode_userdata($order->id.'#'.$order->company_id), 'order_id' => $order->order_id,'serviceProviderId' => jsencode_userdata($order->company_id), 'serviceProvider' => $serviceProvider]);
                     }
                 }
                 $userArray['services'] = $serviceArray;
@@ -47,31 +47,22 @@ class TicketController extends Controller
     }
     public function getUserDetails(Request $request) {
         $userIdArray = [];
-        if($request->userTicket){
-            array_push($userIdArray, jsdecode_userdata($request->userTicket));
-        }
         if($request->companyTicket){
             array_push($userIdArray, jsdecode_userdata($request->companyTicket));
         }
         if(count($userIdArray) > 0){
             try{
-            $companies = User::join('model_has_roles as role', 'role.model_id', '=', 'users.id')->whereIn('id', $userIdArray)->get();
+            $companies = Order::whereIn('id', $userIdArray)->first();
             } catch ( \Exception $e ) {
                 return $this->apiResponse('error', '400', config('constants.ERROR.TRY_AGAIN_ERROR'));
             }
-            $userDetails = [];
-            foreach($companies as $company){
-                $role = Role::where('id', $company->role_id)->first();
-                $userArray = [
-                    'name' => $company->first_name.' '.$company->last_name,
-                    'email' => $company->email,
-                    'role' => $role->name,
-                ];
-                if($role->name == 'Company'){
-                    $userArray['name'] = $company->company_detail->company_name;
-                    $userArray['companySlug'] = $company->company_detail->slug;
+            $userDetails = null;
+            if($companies){
+                $serviceProvider = "OrderNo. #".$companies->order_id." ".$companies->company->company_detail->company_name;
+                if($companies->user_server){
+                    $serviceProvider = $serviceProvider."(".$companies->user_server->domain.")";
                 }
-                array_push($userDetails, $userArray);
+                $userDetails = ['id' => jsencode_userdata($companies->id), 'order_id' => $companies->order_id, 'serviceProviderId' => jsencode_userdata($companies->company_id), 'serviceProvider' => $companies->company->company_detail->company_name, 'serviceBuyer' => $companies->user->first_name.' '.$companies->user->last_name, 'services' => $serviceProvider ];
             }
             return $this->apiResponse('success', '200', 'Data fetched', $userDetails);
         }
