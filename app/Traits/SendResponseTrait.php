d8f4d57bca9d34c7c25926d063f78cb9af208c0f
ShineDezign <gauravch.shinedezign@gmail.com>
2022-02-22 12:17:47 +0530

    resolving api issue

--- app/Traits/SendResponseTrait.php
+++ app/Traits/SendResponseTrait.php
@@ -39,8 +39,8 @@ trait SendResponseTrait {
         $browse_detail = $requestDetail;
         $dectypy_token = jsdecode_api($token);
         $token_array = explode('#', $dectypy_token);
-        return [$browse_detail['ip'].'=='.$token_array[0], $browse_detail['os'].'=='.$token_array[2], $browse_detail['browser'].'=='.$token_array[1]];
-        if(($browse_detail['ip'] == $token_array[0] || $token_array == '127.0.0.1') && $browse_detail['os'] == $token_array[2] && $browse_detail['browser'] == $token_array[1])
+        // return [$browse_detail['ip'].'=='.$token_array[0], $browse_detail['os'].'=='.$token_array[2], $browse_detail['browser'].'=='.$token_array[1]];
+        if(($browse_detail['ip'] == $token_array[0] || $token_array[0] == '127.0.0.1') && $browse_detail['os'] == $token_array[2] && $browse_detail['browser'] == $token_array[1])
         return TRUE;
         else
         return FALSE;
