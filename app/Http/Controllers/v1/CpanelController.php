354647e9d08a9b5dd13e72a6b7e81f5274abd5a3
ShineDezign <gauravch.shinedezign@gmail.com>
2022-03-09 20:33:04 +0530

    update apis

--- app/Http/Controllers/v1/CpanelController.php
+++ app/Http/Controllers/v1/CpanelController.php
@@ -478,12 +478,11 @@ class CpanelController extends Controller
             if(!$serverPackage)
             return response()->json(['api_response' => 'error', 'status_code' => 400, 'data' => 'Connection error', 'message' => config('constants.ERROR.FORBIDDEN_ERROR')]);
             $cpanelStats = $this->getCpanelStats($serverPackage->company_server_package->company_server_id, strtolower($serverPackage->name));
-return $cpanelStats;
             if(!is_array($cpanelStats) ){
                 return response()->json(['api_response' => 'error', 'status_code' => 400, 'data' => 'Connection error', 'message' => config('constants.ERROR.FORBIDDEN_ERROR')]);
             }
-            if ((array_key_exists("result", $cpanelStats) && $cpanelStats["result"]['status'] == "0")) {
-                $error = $cpanelStats["result"]['errors'];
+            if ((array_key_exists("data", $cpanelStats) && $cpanelStats["data"]['status'] == "0")) {
+                $error = $cpanelStats["data"]['reason'];
                 return response()->json(['api_response' => 'error', 'status_code' => 400, 'data' => 'Fetching error', 'message' => $error]);
             }
             $cpanelStatArray = [];
