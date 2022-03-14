fe0353323dd48b9197e70eacaddb3e9657ed0653
ShineDezign <gauravch.shinedezign@gmail.com>
2022-03-14 11:17:17 +0530

    working on ip issue

--- app/Traits/SendResponseTrait.php
+++ app/Traits/SendResponseTrait.php
@@ -39,7 +39,7 @@ trait SendResponseTrait {
         $browse_detail = $requestDetail;
         $dectypy_token = jsdecode_api($token);
         $token_array = explode('#', $dectypy_token);
-        return $requestDetail;
+        return $browse_detail;
         return [$browse_detail['ip'].'=='.$token_array[0], $browse_detail['os'].'=='.$token_array[2], $browse_detail['browser'].'=='.$token_array[1]];
         if($browse_detail['ip'] == $token_array[0] && $browse_detail['os'] == $token_array[2] && $browse_detail['browser'] == $token_array[1])
         return TRUE;
@@ -74,8 +74,8 @@ trait SendResponseTrait {
 
         $data = array (
             'ip' => $this->getClientIp(),
-            'browser' => $browser->browserName() ? :'postman',
-            'os' => $browser->platformName() ? :'postman',
+            'browser' => $browser->browserName()??'postman',
+            'os' => $browser->platformName()??'postman',
         );
         return $data;
     }
