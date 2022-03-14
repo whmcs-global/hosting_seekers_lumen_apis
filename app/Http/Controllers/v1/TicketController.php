f4e4c98d67f2f572044fbcbf99800851b91ac372
ShineDezign <gauravch.shinedezign@gmail.com>
2022-03-14 13:20:44 +0530

    working on api for feedback

--- app/Http/Controllers/v1/TicketController.php
+++ app/Http/Controllers/v1/TicketController.php
@@ -88,6 +88,7 @@ class TicketController extends Controller
             // dd($apiUrl);
             $headers = ['Content-Type: application/json']; 
             $response = hitCurl($apiUrl, 'POST', $request->all() , $headers);
+            return [$apiUrl, $response ];
             $response = json_decode($response, true);
             if($response && $response['success']){
                 return $this->apiResponse('success', '200', 'Feedback has been submitted successfully.');
